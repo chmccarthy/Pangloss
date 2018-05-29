3946528b3e6ab1921676fe9f3c95d45442ca351f
chmccarthy <31311403+chmccarthy@users.noreply.github.com>
2018-05-29 18:08:47 +0100

    Testing
    Former-commit-id: 841591b5c17d0e92b68b54d137df478fbbbbedf0
    Former-commit-id: fd78a84511c129276261fcf7bda6cb5d0f9f1b1c
    Former-commit-id: 66257de6a53f314a69356a2a1ebae36108ff1a2a [formerly 647fa7cb3aea1a76676f60142b9bf653c4117808]
    Former-commit-id: 27ca304466e16e18b0352af8e00697995d24a5ac

---
+++ work/subtelo.py
@@ -0,0 +1,36 @@
+from csv import reader
+
+
+sub_regions = {}
+core = [0, 0]
+acc = [0, 0]
+
+
+with open("chr_sizes.txt") as sizes:
+	for line in sizes.readlines():
+		if line.startswith("ID"):
+			pass
+		else:
+			fields = line.strip("\n").split("\t")
+			first = round(int(fields[1]) * 0.1)
+			second = round(int(fields[1]) * 0.9)
+			sub_regions[fields[0]] = (first, second)
+
+with open("phen_input.txt") as phen:
+	chrs = reader(phen, delimiter="\t")
+	for row in chrs:
+		chr = row[0]
+		start = int(row[1])
+		stop = int(row[2])
+		type = row[3]
+		if sub_regions[chr][0] <= start <= sub_regions[chr][1]:
+			if type == "3":
+				core[0] = core[0] + 1
+			else:
+				acc[0] = acc[0] + 1
+		else:
+			if type == "3":
+				core[1] = core[1] + 1
+			else:
+				print row
+				acc[1] = acc[1] + 1
