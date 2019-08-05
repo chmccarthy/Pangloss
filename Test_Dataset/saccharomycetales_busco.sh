5a39c7079d89e58004fe032f9781d01c757adba4
Charley <Charley.McCarthy@mu.ie>
2019-08-05 13:22:03 +0100

    Adding in test data and updated manual

---
+++ Test_Dataset/saccharomycetales_busco.sh
@@ -0,0 +1,4 @@
+#The saccharomycetales BUSCO dataset is too large to put on GitHub so just
+#run this script to download it.
+
+curl -O https://busco.ezlab.org/datasets/saccharomycetales_odb9.tar.gz
