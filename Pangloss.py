a4900b65edfe68e09709a186bc0b659937b34b52
Charley <Charley.McCarthy@mu.ie>
2019-08-05 13:00:46 +0100

    Update Pangloss.py

--- Pangloss.py
+++ Pangloss.py
@@ -9,10 +9,15 @@ See Pangloss/README.md for more information.
 
 To-do:
     - Improve logging.
-    - Seriously improve config file!
-    - Write manual!
 
 Recent changes:
+    v0.9.1 (August 2019)
+    - Fixes to how Pangloss reads in config file.
+    - Made Pangloss.py executable.
+    - Fixes to yn00 parsing.
+    - Disabled tarballing prediction temp files because of how slow it seemed to be.
+    - Other minor changes.
+
     v0.9.0 (June 2019)
     - Tested out InterProScan and GOATools pipelines.
     - Changed how UpSet plots are generated, now done through Cairo (on Mac at least).
@@ -157,40 +162,39 @@ def PanGuessHandler(ex_path, gm_path, tp_path, tl_path,
         
         # Run prediction using GeneMark-ES.
         logging.info("Master: Running gene model prediction for {0} using GeneMark-ES.".format(genome))
-        #genemark_gtf = PanGuess.RunGeneMark(genome, gm_path, gm_branch, cores)
+        genemark_gtf = PanGuess.RunGeneMark(genome, gm_path, gm_branch, cores)
         
         # Convert GeneMark-ES GTF file into a more PanOCT-compatible version.
         logging.info("Master: Converting GeneMark GTF data to attribute data.")
-        #genemark_attributes = PanGuess.GeneMarkGTFConverter(genemark_gtf, tag)
+        genemark_attributes = PanGuess.GeneMarkGTFConverter(genemark_gtf, tag)
         
         # Merge unique gene model calls between Exonerate and GeneMark-ES.
         if not skip:
             logging.info("Master: Merging Exonerate and GeneMark-ES gene calls.")
-        #    merged_attributes = PanGuess.MergeAttributes(exonerate_attributes, genemark_attributes)
+            merged_attributes = PanGuess.MergeAttributes(exonerate_attributes, genemark_attributes)
         else:
-            pass
-            #merged_attributes = genemark_attributes
+            merged_attributes = genemark_attributes
             del genemark_attributes
 
         # Clean up GeneMark-ES files and folders.
         logging.info("Master: Tidying up GeneMark-ES temporary files.")
-        #PanGuess.MoveGeneMarkFiles(workdir, genome)
+        PanGuess.MoveGeneMarkFiles(workdir, genome)
         
         # Extract NCRs into list.
         logging.info("Master: Extracting non-coding regions of {0} for TransDecoder analysis.".format(genome))
-        #noncoding = PanGuess.ExtractNCR(exonerate_attributes, genome)
+        noncoding = PanGuess.ExtractNCR(exonerate_attributes, genome)
         
         # Run TransDecoder on NCRs.
         logging.info("Master: Running TransDecoder on non-coding regions of {0}.".format(genome))
-        #tdir = PanGuess.RunTransDecoder(noncoding, tp_path, tl_path, workdir, genome, td_len)
+        tdir = PanGuess.RunTransDecoder(noncoding, tp_path, tl_path, workdir, genome, td_len)
         
         # Move TransDecoder files.
         logging.info("Master: Tidying up TransDecoder temporary files.")
-        #PanGuess.MoveTransDecoderFiles(tdir)
+        PanGuess.MoveTransDecoderFiles(tdir)
         
         # Extract TransDecoder attributes.
         logging.info("Master: Converting TransDecoder GTF data to attribute data.")
-        #trans_attributes = PanGuess.TransDecoderGTFToAttributes(tdir, tag)
+        trans_attributes = PanGuess.TransDecoderGTFToAttributes(tdir, tag)
 
         # Merge TransDecoder calls into the Exonerate + GeneMark-ES set.
         logging.info("Master: Merging all remmaining gene calls for {0}.".format(genome))
